bbf9afcb627b70febf2f07212a598ac9d3ab3ab8
Chabry <chabry01@gmail.com>
2024-11-22 15:25:16 +0100

    Exercice résolu

--- 03-07-data-types-objects.js
+++ 03-07-data-types-objects.js
@@ -3,26 +3,42 @@
 // Function 1
 function createObject(key1, value1, key2, value2) {
   // Your code here
+  const obj = {
+    [key1]: value1,
+    [key2]: value2,
+  }
+
+  return obj
 }
 
 // Function 2
 function accessPropertyValue(obj, property) {
   // Your code here
+  return obj[property]
 }
 
 // Function 3
 function checkProperty(obj, property) {
   // Your code here
+  if(obj[property]){
+    return true
+  }else{
+    return false
+  }
 }
 
 // Function 4
 function addProperty(obj, property, value) {
   // Your code here
+  obj[property] = value;
+  return obj
 }
 
 // Function 5
 function removeProperty(obj, property) {
   // Your code here
+  delete obj[property]
+  return obj
 }
 
 module.exports = {
